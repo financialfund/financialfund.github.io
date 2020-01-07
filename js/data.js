8bef3a092c4b2aad16478061b453bc923d79c78c
financialfund <58726815+financialfund@users.noreply.github.com>
2020-01-07 17:09:42 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,12 +2,12 @@
 	"2019 October", 	"2019 October", 	"2019 October", 	"2019 October", \
 	"2019 November", 	"2019 November", 	"2019 November", 	"2019 November", \
 	"2019 December", 	"2019 December",	"2019 December",	"2019 December",	"2019 December",\
-	"2020 January",\
+	"2020 January"\
 	],"balance":[\
 	1000,			1040, 			1080, 			1119.23, \
 	1131.23,		1198.11, 		1244.25, 		1286.73, \
 	1254.76, 		1330.57,		1413.83,		1386.91,		1435.02,\
-	1517.08,\
+	1517.08\
 	]}';
 	var myObj = JSON.parse(myJSON);
 	
